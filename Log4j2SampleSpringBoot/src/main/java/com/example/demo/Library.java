e41641d88c456614bc42d27e340d675af2a7de7c
Vibhor <bhardwaj.vibhor1@gmail.com>
2017-12-17 09:19:35 +0530

    Log4j with spring boot

---
+++ Log4j2SampleSpringBoot/src/main/java/com/example/demo/Library.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+public class Library {
+	public static void main(String[] args) {
+			 ExecutorService executor = Executors.newFixedThreadPool(3);
+		        for (int i = 0; i < 10; i++) {
+		        	Runnable triggerConsumer = new TriggerConsumer(new Trigger(i,i,"sourcename-"+i));
+		            executor.submit(triggerConsumer);
+		        }
+		        executor.shutdown();
+
+	}
+
+}
